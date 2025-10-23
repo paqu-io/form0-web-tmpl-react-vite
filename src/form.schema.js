16f3403169a842cf3886a0638f3bc8c1e74f913a
Stefano Pompei <35841717+aragornii@users.noreply.github.com>
2025-10-23 13:59:23 +0200

    feat(schema): enhance form schema with additional properties and conditions for fields

--- src/form.schema.js
+++ src/form.schema.js
@@ -8,103 +8,171 @@ export default {
         data_name: 'personal_info',
         label: 'Personal Info',
         display: 'inline', //Section can be 'inline' or 'drilldown'
+        description: null, //description can be null or a string
+        description_mode: null, //description_mode can be null,'default' or 'subtext'
+        visible: true,
+        visible_conditions: null,
         elements: [
           {
             type: 'TextField',
             data_name: 'first_name',
             label: 'First Name',
+            display: 'default', //TextField can only be 'default'
+            description: null, //description can be null or a string
+            description_mode: null, //description_mode can be null, 'default' or 'subtext'
             required: true,
-            hidden: false,
+            required_conditions: null,
+            visible: true,
+            visible_conditions: null,
             read_only: false,
+            read_only_conditions: null,
+            default_value: null,
             pattern: '^[a-zA-Z]+$',
-            pattern_description: 'One or more letters (uppercase or lowercase), with no spaces, numbers, or symbols'
+            pattern_description:
+              'One or more letters (uppercase or lowercase), with no spaces, numbers, or symbols',
+            supporting_image: false, //supporting_image can be true or false
+            supporting_image_path: null, //supporting_image_path can be null or a string
+            supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
           },
           {
             type: 'NumericField',
             data_name: 'age',
             label: 'Age',
+            display: 'default', //NumericField can only be 'default'
+            description: null, //description can be null or a string
+            description_mode: null, //description_mode can be null, 'default' or 'subtext'
             required: true,
-            hidden: false,
+            required_conditions: null,
+            visible: true,
+            visible_conditions: null,
             read_only: false,
+            read_only_conditions: null,
+            default_value: null,
             min: 16,
             max: 100,
             format: 'integer', //NumericField can be 'integer' or 'float'
-          }
+            supporting_image: false, //supporting_image can be true or false
+            supporting_image_path: null, //supporting_image_path can be null or a string
+            supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
+          },
         ]
       },
       {
         type: 'CalculatedField',
         data_name: 'can_vote',
         label: 'Eligible',
-        required: false, //CalcualtedField is required = false by default
-        hidden: false,
-        read_only: true, //CalcualtedField is read_only = true by default
-        calculate: 'IF($age >= 18, "yes", "no")',
         display: {
-            style: 'text' // or numeric, date, currency
-        }
+          style: 'text', // or numeric, date, currency
+        },
+        description: null, //description can be null or a string
+        description_mode: null, //description_mode can be null, 'default' or 'subtext'
+        required: false, //CalcualtedField is always required = false
+        visible: true,
+        visible_conditions: null,
+        read_only: true, //CalcualtedField is always read_only = true
+        calculate: 'IF($age >= 18, "yes", "no")',
+        supporting_image: false, //supporting_image can be true or false
+        supporting_image_path: null, //supporting_image_path can be null or a string
+        supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
       },
       {
-          type: 'CalculatedField',
-          data_name: 'calc_test',
-          label: 'calc_test',
-          required: false, //CalcualtedField is required = false by default
-          hidden: false,
-          read_only: true, //CalcualtedField is read_only = true by default
-          calculate: 'SETRESULT($age + 10 >= 30 ? true : false)',
-          display: {
-              style: 'text' // or numeric, date, currency
-          }
+        type: 'CalculatedField',
+        data_name: 'calc_test',
+        label: 'calc_test',
+        display: {
+          style: 'text', // or numeric, date, currency
+        },
+        description: null, //description can be null or a string
+        description_mode: null, //description_mode can be null, 'default' or 'subtext'
+        required: false, //CalcualtedField is always required = false
+        visible: true,
+        visible_conditions: null,
+        read_only: true, //CalcualtedField is always read_only = true
+        calculate: 'SETRESULT($age + 10 >= 30 ? true : false)',
+        supporting_image: false, //supporting_image can be true or false
+        supporting_image_path: null, //supporting_image_path can be null or a string
+        supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
       },
       {
-          type: 'CalculatedField',
-          data_name: 'calc_test_new',
-          label: 'calc_test_new',
-          required: false, //CalcualtedField is required = false by default
-          hidden: false,
-          read_only: true, //CalcualtedField is read_only = true by default
-          calculate: '$age + 88',
-          display: {
-              style: 'text' // or numeric, date, currency
-          }
+        type: 'CalculatedField',
+        data_name: 'calc_test_new',
+        label: 'calc_test_new',
+        display: {
+          style: 'text', // or numeric, date, currency
+        },
+        description: null, //description can be null or a string
+        description_mode: null, //description_mode can be null, 'default' or 'subtext'
+        required: false, //CalcualtedField is always required = false
+        visible: true,
+        visible_conditions: null,
+        read_only: true, //CalcualtedField is always read_only = true
+        calculate: '$age + 88',
+        supporting_image: false, //supporting_image can be true or false
+        supporting_image_path: null, //supporting_image_path can be null or a string
+        supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
       },
       {
-          type: 'Section',
-          data_name: 'section_drill',
-          label: 'Drilldown section test',
-          display: 'drilldown', //Section can be 'inline' or 'drilldown'
-          elements: [
-            {
-              type: 'TextField',
-              data_name: 'comments',
-              label: 'Comments',
-              required: false,
-              hidden: false,
-              read_only: false,
-              pattern: null,
-              pattern_description: null
-            }
+        type: 'Section',
+        data_name: 'section_drill',
+        label: 'Drilldown section test',
+        display: 'drilldown', //Section can be 'inline' or 'drilldown'
+        description: null, //description can be null or a string
+        description_mode: null, //description_mode can be null, 'default' or 'subtext'
+        visible: true,
+        visible_conditions: null,
+        elements: [
+          {
+            type: 'TextField',
+            data_name: 'comments',
+            label: 'Comments',
+            display: 'default', //TextField can only be 'default'
+            description: null, //description can be null or a string
+            description_mode: null, //description_mode can be null, 'default' or 'subtext' 
+            required: false,
+            required_conditions: null,
+            visible: true,
+            visible_conditions: null,
+            read_only: false,
+            read_only_conditions: null,
+            default_value: null,
+            pattern: null,
+            pattern_description: null,
+            supporting_image: false, //supporting_image can be true or false
+            supporting_image_path: null, //supporting_image_path can be null or a string
+            supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
+          },
           ]
       },
       {
-          type: 'TextField',
-          data_name: 'who_voted',
-          label: 'Who voted?',
-          required: true,
-          read_only: true,
-          hidden: false,
-          visible_conditions: {
-              and: [
-                { field_key: 'can_vote', operator: 'equal_to', value: 'yes' },
-                {
-                  or: [
-                    { field_key: 'age', operator: 'greater_than', value: 20 },
-                    { field_key: 'first_name', operator: 'equal_to', value: 'Bob' }
-                  ]
-                }
-              ]
-          }
-      }
+        type: 'TextField',
+        data_name: 'who_voted',
+        label: 'Who voted?',
+        display: 'default', //TextField can only be 'default'
+        description: null, //description can be null or a string
+        description_mode: null, //description_mode can be null, 'default' or 'subtext'
+        required: true,
+        required_conditions: null,
+        visible: false,
+        visible_conditions: {
+          and: [
+            { field_id: 'can_vote', operator: 'equal_to', value: 'yes' },
+            {
+              or: [
+                { field_id: 'age', operator: 'greater_than', value: 20 },
+                { field_id: 'first_name', operator: 'equal_to', value: 'Bob' },
+              ],
+            },
+          ],
+        },
+        read_only: true,
+        read_only_conditions: null,
+        default_value: null,
+        pattern: null,
+        pattern_description: null,
+        supporting_image: false, //supporting_image can be true or false
+        supporting_image_path: null, //supporting_image_path can be null or a string
+        supporting_image_display: null, //supporting_image_display can be 'default', 'dialog' or null
+      },
     ]
   }
 };
